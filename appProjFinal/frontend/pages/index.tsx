d9d1426e9a58a22eeb6ff1e1c2d9dc26daf0f620
David da Costa Coimbra <88290682+DavidCCoimbra@users.noreply.github.com>
2023-05-06 00:48:02 +0100

    Comunicacao_API_Backend_Frontend

--- appProjFinal/frontend/pages/index.tsx
+++ appProjFinal/frontend/pages/index.tsx
@@ -1,19 +1,34 @@
-import BottomButton from './components/BottomButton'
-import Menu from './components/Menu'
-import CategoryButton from './components/CategoryButton'
-import { Inter } from 'next/font/google'
-import { Button, Flex, Image, Text } from "@chakra-ui/react";
-import Sidebar from './components/Sidebar';
+import BottomButton from "./components/BottomButton";
+import Menu from "./components/Menu";
+import Sidebar from "./components/Sidebar";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { GET_ALL_CATEGORIES } from "@/graphql/queries";
 
-
-
-export default function Home() {
+export default function Home({categories}) {
+  console.log(categories);
   return (
     <>
-    <Menu />
-    <Sidebar/>
-    {/*<Image src="/images/bg_inicio.jfif" backgroundSize={"cover"} h="80vh" w="100vw" alt="Logo" />*/}
-    <BottomButton />
+      <Menu />
+      <Sidebar />
+      {/*<Image src="/images/bg_inicio.jfif" backgroundSize={"cover"} h="80vh" w="100vw" alt="Logo" />*/}
+      <BottomButton />
     </>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const client = new ApolloClient({
+    uri: process.env.STRAPI_GRAPHQL_API,
+    cache: new InMemoryCache(),
+  });
+
+  const { data } = await client.query({
+    query: GET_ALL_CATEGORIES,
+  });
+
+  return {
+    props: {
+      categories: data.categories.data,
+    },
+  };
 }
